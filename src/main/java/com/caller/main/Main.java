c5e813e5e8583ef24598e03e55a559c3867b0f6c
Moshe Cohen <moshec@gmail.com>
2023-10-06 22:13:21 -0400

    added 'shade' goal to pom

--- src/main/java/com/caller/main/Main.java
+++ src/main/java/com/caller/main/Main.java
@@ -6,9 +6,6 @@ public class Main {
     static public void main(String[] args) {
 	System.out.println("Hi there ");
 
-	Noop x = null;
-	System.out.println(x);
-	x = new Noop();
 	String s = Noop.noop();
 	System.out.println("caller: received '" + s + "' from noop");
     }

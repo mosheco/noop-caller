7b1af8def5115201bab2b98370720638a0589a09
Moshe Cohen <moshec@gmail.com>
2023-10-06 21:18:46 -0400

    1

--- src/main/java/com/caller/main/Main.java
+++ src/main/java/com/caller/main/Main.java
@@ -1,7 +1,15 @@
 package com.caller.main;
 
+import com.backslash.noop.Noop;
+
 public class Main {
-  static public void main(String[] args) {
-    System.out.println("Hi there ");
-  }
+    static public void main(String[] args) {
+	System.out.println("Hi there ");
+
+	Noop x = null;
+	System.out.println(x);
+	x = new Noop();
+	String s = Noop.noop();
+	System.out.println("caller: received " + s + " from noop");
+    }
 }
